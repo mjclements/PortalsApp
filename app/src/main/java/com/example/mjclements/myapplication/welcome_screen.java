c782c0dfd631023ef6bf9e4a1083bf3758ad1d76
mjclements <mjclements@wpi.edu>
2019-01-06 14:22:54 -0500

    Fixed a rather nasty naming mismatch.

--- app/src/main/java/com/example/mjclements/myapplication/welcome_screen.java
+++ app/src/main/java/com/example/mjclements/myapplication/welcome_screen.java
@@ -2,6 +2,7 @@ package com.example.mjclements.myapplication;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.content.Intent;
 
